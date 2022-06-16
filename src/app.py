2dde5cec16eaf2f715c4be5610f1acc32135409b
Nkechi Asuai <nasuai3880@gmail.com>
2022-06-16 05:34:15 +0100

    added code successfully

--- src/app.py
+++ src/app.py
@@ -3,7 +3,26 @@ import os
 
 
 def prime(s):
-    # your code goes here
+    
+# A program to check if a number is prime or not
+    # To take  input from the user
+
+    num = int(input("Enter a value: "))
+
+    # prime numbers are greater than 1
+    if num > 1:
+
+        for n in range(2, num):
+            if (num % n) == 0:
+                print(num, "is not a prime number")
+
+                break
+
+        else:
+            print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
+
 
 def solution(s):
     return prime(s)
